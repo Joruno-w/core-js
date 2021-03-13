88bb843cb5763bc84b685684b419e856d008c266
Denis Pushkarev <zloirock@zloirock.ru>
2021-03-14 04:00:38 +0700

    add `/(es|stable|actual)/(get-iterator|get-iterator-method|is-iterable)` entries

--- tests/commonjs.mjs
+++ tests/commonjs.mjs
@@ -792,12 +792,5 @@ PATH = 'core-js';
 load('bundle/actual');
 load('bundle/full');
 
-PATH = 'core-js-bundle';
-load('');
-load('actual');
-load('actual.min');
-load('full');
-load('full.min');
-
 // eslint-disable-next-line no-console -- output
 console.log(chalk.green(`tested ${ chalk.cyan(tested) } commonjs entry points`));
