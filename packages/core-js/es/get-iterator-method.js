2284bfaac4d8ba167ecc990edb49b515e510579f
Denis Pushkarev <zloirock@zloirock.ru>
2021-08-05 01:54:19 +0700

    update after rebasing

--- packages/core-js/es/get-iterator-method.js
+++ packages/core-js/es/get-iterator-method.js
@@ -3,5 +3,4 @@ require('../modules/es.string.iterator');
 require('../modules/web.dom-collections.iterator');
 var getIteratorMethod = require('../internals/get-iterator-method');
 
-
 module.exports = getIteratorMethod;
